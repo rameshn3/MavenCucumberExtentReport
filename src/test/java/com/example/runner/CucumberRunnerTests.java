534dd1313265d3a63497ce70becd32bd3bbcd561
Ramesh <rameshn3@gmail.com>
2025-09-28 07:47:19 -0500

    bdd cucumber testcases

--- src/test/java/com/example/runner/CucumberRunnerTests.java
+++ src/test/java/com/example/runner/CucumberRunnerTests.java
@@ -3,7 +3,7 @@ package com.example.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags = "", features = "src/test/resources/features/LoginPage.feature", 
+@CucumberOptions(tags = "", features = "src/test/resources/features",
 glue = "com.example.definitions", 
 plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 
